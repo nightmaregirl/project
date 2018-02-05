33ee6c2c62d716cbce6e523886d897d322d9958f
KH037s <KH037s@KH037-14.uncanet.unca.edu>
2018-02-05 18:23:58 -0500

    aDDED A BOWTIE AND SMILES.

---
+++ src/MyName.java
@@ -0,0 +1,19 @@
+
+public class MyName {
+
+
+	public static void main(String[] args) { System.out.println
+		   ("RRRR    (◕‿◕✿)          ôヮô  MM        MM    \r\n" + 
+			"RRRRRR  (◕‿◕✿)          ôヮô  MMMM     MMMM     \r\n" + 
+			"RR  RR  (◕‿◕✿)          ôヮô  MM MM   MM MM      \r\n" + 
+			"RRRRR   (◕‿◕✿)    	ôヮô  MM  MM MM   MM  \r\n" + 
+			"RRR     (◕‿◕✿)(ꈍ ˬ ꈍ✿) ôヮô  MM   MMM    MM       \r\n" + 
+			"RR RR   (◕‿◕✿)          ôヮô MM    MM     MM \r\n" + 
+			"RR  RR  (◕‿◕✿)          ôヮô MM           MM    \r\n" + 
+			"RR   RR (◕‿◕✿)          ôヮô MM           MM       \r\n" + 
+			"RR    RR(◕‿◕✿)          ôヮô MM           MM"          );
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/bowtie.java
@@ -0,0 +1,18 @@
+
+public class bowtie {
+
+	public static void main(String[] args) {System.out.println ("*********");
+	System.out.println(" *******");
+	System.out.println ("  *****");
+	System.out.println("   ***");
+	System.out.println("    *");
+	System.out.println ("   ***");
+	System.out.println  ("  *****");
+	System.out.println(" *******");
+	System.out.println	(
+"*********");
+		// TODO Auto-generated method stub
+
+	}
+
+}
